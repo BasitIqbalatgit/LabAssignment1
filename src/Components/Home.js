fe5964a802cd7a94f1aefb080a671d2ae21bf42c
BASIT <rai.xardar.050@gmail.com>
2023-10-25 11:00:29 +0500

    pushing home

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Welllcome To Home Screen</Text>
+      <Button>Task 1 </Button>
+      <Button>Task 2</Button>
+      <Button>Task 3</Button>
+    </View>
+  )
+}
+
+export default Home
