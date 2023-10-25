665490a1a496337ae709ba567b365605f0697438
Fatima Aftab <129087237+fatimaaftab1388@users.noreply.github.com>
2023-10-25 14:03:16 +0500

    fati

--- src/Components/Homes.js
+++ src/Components/Homes.js
@@ -1,16 +1,21 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
 
 const Homes = ({navigation}) => {
   return (
+    <ImageBackground
+    style={styles.back}
+    source={require('../images/3.png')}
+    resizeMode='stretch'>
     <View style={styles.container}>
-      <Text>Welllcome To Home Screen</Text>
+      <Text style={styles.t}>Welllcome To Home Screen</Text>
       <View style={styles.inline}>
       <Button title='Task 1 ' onPress={()=>navigation.navigate('Login')} />
       <Button title='Task 2 ' onPress={()=>navigation.navigate('TaskForm')}/>
       <Button title='Task 3 ' onPress={()=>navigation.navigate('Calculator')}/>
       </View>
     </View>
+    </ImageBackground>
   )
 }
 
@@ -25,7 +30,19 @@ const styles= StyleSheet.create({
     flexDirection:"row",
     justifyContent:"space-between"
 
-  }
+  },
+
+  t:{
+    color:'skyblue',
+    fontSize:30,
+  marginBottom:10
+  },
+  back: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+
 
 })
 
